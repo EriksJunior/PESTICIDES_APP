8728d9605a08296dac94bf21fb112b1baa61da7a
eriks junior <eriksjunior13@gmail.com>
2024-10-31 20:47:58 -0300

    refact

---
+++ app/(search)/_layout.tsx
@@ -0,0 +1,27 @@
+import { Tabs } from 'expo-router';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{
+      tabBarActiveTintColor: 'green',
+    }}>
+      <Tabs.Screen name="index" options={{
+        title: 'Agrotóxicos',
+        headerShown: false,
+        tabBarIcon: ({ color, focused }) => (
+          <MaterialCommunityIcons name={focused ? 'leaf-circle' : 'leaf-circle-outline'} color={color} size={30} />
+        )
+      }} />
+
+      <Tabs.Screen name="about" options={{
+        title: 'Atualizações',
+        headerShown: false,
+        tabBarIcon: ({ color, focused }) => (
+          <FontAwesome6 name={focused ? 'book-open' : 'book'} color={color} size={24} />
+        ),
+      }} />
+    </Tabs>
+  );
+}
