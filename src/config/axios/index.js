b50554e62d07712fccd822f307f4a4d98807877f
eriks junior <eriksjunior13@gmail.com>
2025-02-07 09:28:30 -0300

    refact

--- src/config/axios/index.js
+++ src/config/axios/index.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://8ee1-2804-53e0-9304-4e00-bd45-b2da-b422-a4ec.ngrok-free.app",
-  headers: {
-    header: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiMzcwNzgwMzMtOGE1Yi00ZjQzLTkyODQtMmNlMzA3NDg0MDdiIiwiaWF0IjoxNzM1ODU0MTQ5LCJleHAiOjE3MzU5NDA1NDl9.PWy_xHTKK0C0qwqQOk3G8OgamKZrTpEaQixhpOkvzJ0"
-  }
+  baseURL: "https://8b64-177-129-55-109.ngrok-free.app",
 })
